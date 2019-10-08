7c60b8f6762dce70cf367d628fe3dc9f425d8fa4
LivinBreezy <robertjs@hfx.eastlink.ca>
2019-10-08 14:29:37 -0300

    Updated documentation for new version.

--- documentation/navtreedata.js
+++ documentation/navtreedata.js
@@ -22,32 +22,12 @@ for the JavaScript code in this file
 */
 var NAVTREE =
 [
-  [ "TPG++", "index.html", [
-    [ "Todo List", "dd/da0/todo.html", null ],
-    [ "Bug List", "de/da5/bug.html", null ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ],
-        [ "Typedefs", "globals_type.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "TPG++", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
